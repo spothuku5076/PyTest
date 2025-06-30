1282d3b6d6d252986519e5f4f6f1ac0c329e80f8
Sai Lalitesh Pothukuchi <spothuku5076@Sais-MacBook-Pro.local>
2025-06-29 17:58:45 -0700

    final pytest

--- source/greet.py
+++ source/greet.py
@@ -1,6 +1,12 @@
+import logging
+
+
+# logging.basicConfig(filename='log.txt', level=logging.info)
+
 class Greet:
     """This class is for Greet
     """
+    logging.info("This is a greet class")
     def greet(self,name):
         return f"Hello {name}"
     
